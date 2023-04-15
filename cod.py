0e75f11201bdb66e9b3d2165bf46be5bdf7a2d96
ЦУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУУССССССССССССССССССССССССССССССССССССССССКККККККККККККККККККУУУ <130209894+qwe123qwe123qwe123qwe123qwe123@users.noreply.github.com>
2023-04-15 13:30:44 +0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -3,7 +3,9 @@ import time
 import random
 
 pygame.init() 
-pygame.font.init() 
+pygame.font.init()
+
+
  
 class img: 
  
@@ -17,12 +19,53 @@ class img:
     def collidepoint(self, x, y): 
         return self.rect.collidepoint(x, y)
         
+bg = img("bg.png", 0,0,800,600)
+car = img("car.png", 0,0,400,150)
+car2 = img("carpovorot.png", 0,0,400,150)
+kamen = img("kamen.png", 300,350,200,150)
 
-sc = pygame.display.set_mode((500, 500)) 
+carga = random.randint(1,3)
+
+
+carx = 800
+cary = 400
+
+car1x = -400
+car1y = 400
+
+sc = pygame.display.set_mode((800, 600)) 
 clk = pygame.time.Clock()
-
+clk.tick(60)
 game = True 
 while game:
     for i in pygame.event.get(): 
         if i.type == pygame.QUIT: 
             game = False
+
+    bg.draw(0,0)
+    kamen.draw(300,425)
+
+    
+    if carga == 1:
+        carx -=10
+        print(carx)
+        if carx <= -400:
+            carx = 900
+            carga = random.randint(1,2)
+
+
+    if carga == 2:
+        car1x +=10
+        print(car1x)
+        if car1x >= 800:
+            car1x = -500
+            carga = random.randint(1,2)
+
+    car.draw(carx,cary)
+    car2.draw(car1x,car1y)
+
+
+    clk.tick(60)
+    pygame.display.update()
+
+pygame.QUIT
